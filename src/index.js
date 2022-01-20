b7559238d992054dae83383143e2d04bbfc20d08
jeremyacoop <63517273+jeremyacoop@users.noreply.github.com>
2022-01-20 13:43:44 -0600

    Set up reducer templates

--- src/index.js
+++ src/index.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createStore, combineReducers, applyMiddleware } from 'redux';
+import { Provider } from 'react';
 import './index.css';
+
 import App from './components/App/App';
 
-ReactDOM.render(<App />, document.getElementById('root'));
 
-//create a reducer for order
-//when you click add, the components added should be visible in cart
-//CART
+// MENU REDUCERS
+// - List of pizzas available for order
+// - This is rendered on screen one
 
 //create a reducer for pizza list
 // when we load the order page, we will say render a menu
@@ -15,9 +17,13 @@ ReactDOM.render(<App />, document.getElementById('root'));
 //create a reducer for customer info (see screen two)
 //takes customer info and
 
-// MENU REDUCERS
-// - List of pizzas available for order
-// - This is rendered on screen one
+const menuReducer = (state = [], action) => {
+    switch (action.type) {
+        default:
+            return state;
+    }
+
+}
 
 // CART/ORDER REDUCER
 // - What current user/orderer has in their cart
@@ -29,12 +35,31 @@ ReactDOM.render(<App />, document.getElementById('root'));
     }
 */
 
+//create a reducer for order
+//when you click add, the components added should be visible in cart
+//CART
+
+const checkoutReducer = (state = [], action) => {
+    switch (action.type) {
+        default:
+            return state;
+    }
+
+}
 
 // CUSTOMER INFO REDUCER
 // - Screen two
 //      - name, address, city, zip
 //      { name: name, address: address, city: city, zip: zip }
 //      - delivery or pickup
+
+const customerReducer = (state = [], action) => {
+    switch (action.type) {
+        default:
+            return state;
+    }
+
+}
 
 /*
 // ADMIN/ACTIVE ORDERS
@@ -50,3 +75,12 @@ ReactDOM.render(<App />, document.getElementById('root'));
     }
 
 */
+
+// const adminReducer = (state = [], action) => {
+//     switch (action.type) {
+//         default:
+//             return state;
+//     }
+// }
+
+ReactDOM.render(<Provider><App /></Provider>, document.getElementById('root'));
